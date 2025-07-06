a32c19920e44f916707b2325e049341a94608be2
Andreas Schjønhaug <andreas@schjonhaug.com>
2025-07-06 21:55:12 +0200

    compose

--- schjonhaug-kanari/exports.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Tor hidden service
-hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}/hostname"
-export APP_KANARI_HIDDEN_SERVICE="$(cat "${hidden_service_file}" 2>/dev/null || echo "")"
-
-# electrs is a required dependency, so it will always be available
-export APP_KANARI_ELECTRUM_URL="tcp://$APP_ELECTRS_NODE_IP:$APP_ELECTRS_NODE_PORT"
